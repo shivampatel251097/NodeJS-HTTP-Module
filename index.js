5a938ad0764efc29b661f6c6d2db6a5a9efd6823
Shivam Patel <shivampatel251097@gmail.com>
2020-11-18 16:54:57 +0530

    Node HTTP Module Example2

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 const http = require('http');
-
+const fs =  require('fs');
+const path = require('path');
 const hostname = 'localhost';
-const port = 3001;
+const port = 3000;
 
 const server = http.createServer((req,res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>Hello World!!</h1></body></html>');
+    console.log("Request for "+ req.url +" by method " + req.method);
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type','text/html');
+    // res.end('<html><body><h1>Hello World!!</h1></body></html>');
+    if(req.method == 'GET'){
+        var fileURL;
+        if (req.url == '/'){
+            fileURL = '/index.html';
+        }
+        else{
+            fileURL = req.url;
+        }
+        var filePath = path.resolve('./public'+fileURL);
+        const fileExt = path.extname(filePath);
+        if (fileExt == '.html'){
+            fs.stat(filePath, (err,stat)=> {
+                if(err == null)  {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type','text/html');
+                    fs.createReadStream(filePath).pipe(res);
+                    return;
+                }
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type','text/html');
+                    res.end('<html><body><h1>Error 404: '+fileURL+' not found</h1></body></html>');
+            })
+        }
+        else{
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type','text/html');
+                    res.end('<html><body><h1>Error 404: '+fileURL+' not an HTML file</h1></body></html>');
+                    return;
+        }
+
+    }
+    else{
+        res.statusCode = 404;
+        res.setHeader('Content-Type','text/html');
+        res.end('<html><body><h1>Error 404: '+req.method+' not supported file</h1></body></html>');
+        return;
+    }
 })
 
 server.listen(port,hostname,()=> {
